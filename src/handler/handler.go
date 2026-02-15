c2a349d944565256bb888732807e98e17bb06274
Loic.Servajean <loic.servajean@outlook.fr>
2026-02-15 16:57:10 +0100

    feat: ajout de la recherche de joueur
    seul le nom exact marche j'ai tester avec laaazi

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -2,8 +2,12 @@ package handler
 
 import (
 	"SCORESABER--New/src/service"
+	"encoding/json"
+	"errors"
 	"html/template"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 type PlayerPageData struct {
@@ -14,6 +18,20 @@ type PlayerPageData struct {
 func PlayerPageHandler(w http.ResponseWriter, r *http.Request) {
 
 	id := r.URL.Query().Get("id")
+
+	if id == "" {
+		http.Error(w, "Aucun nom fourni", http.StatusBadRequest)
+		return
+	}
+
+	if !isNum(id) {
+		n, err := searchName(id)
+		if err != nil {
+			http.Error(w, "Joueur introuvable", http.StatusNotFound)
+			return
+		}
+		id = n
+	}
 
 	user, status, err := service.GetUser(id)
 	if err != nil || status != http.StatusOK {
@@ -34,4 +52,34 @@ func PlayerPageHandler(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("html/Player.html")
 	t.Execute(w, data)
+}
+
+func isNum(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func searchName(name string) (string, error) {
+	u := "https://scoresaber.com/api/players?search=" + url.QueryEscape(name)
+	resp, err := http.Get(u)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var d struct {
+		Players []struct {
+			ID string `json:"id"`
+		} `json:"players"`
+	}
+
+	if json.NewDecoder(resp.Body).Decode(&d) != nil {
+		return "", errors.New("decode error")
+	}
+
+	if len(d.Players) == 0 {
+		return "", errors.New("not found")
+	}
+
+	return d.Players[0].ID, nil
 }
